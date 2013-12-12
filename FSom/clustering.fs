97414bca79502ee6f59f16346180ccd3e824b5de
Boris <fierval@gmail.com>
2013-12-11 20:55:03 -0800

    fixing the over-clustering problem

--- FSom/clustering.fs
+++ FSom/clustering.fs
@@ -150,7 +150,7 @@ module Clustering =
               
     ///Breaks the U*-Matrix into watersheds by gradually
     /// "immersing" it into water                                  
-    type Watershed (uMatrix: float [,]) =
+     type Watershed (uMatrix: float [,]) =
         let uMatrix = uMatrix
         let width = uMatrix |> Array2D.length2
         let height = uMatrix |> Array2D.length1
@@ -159,6 +159,10 @@ module Clustering =
         let minimalpoints = Array2D.zeroCreate height width
         let localmins = Array.zeroCreate (height * width) 
         let mutable basins = 0
+
+        let labelToPoints = Dictionary<int, System.Collections.Generic.List<Point>>()
+        do
+            labelToPoints.Add(0, List<Point>())
 
         /// given a u-matrix retrun a map of its heights
         let getOrderedHeights () =
@@ -174,9 +178,14 @@ module Clustering =
             heightMap.OrderBy(fun kvp -> kvp.Key)
 
         let processShed (point : Point) =
+            let r = point.x
+            let c = point.y
+            let curHeight = uMatrix.[r, c]
+
             let neighbors = point.getNeighbors width height
             
             let availableLabels = HashSet(HashIdentity.Structural)
+            let misassignedLabels = HashSet(HashIdentity.Structural)
 
             let mutable minNeighbor = Unchecked.defaultof<Point>
             let mutable minNeighborHeight = Double.MaxValue
@@ -187,35 +196,67 @@ module Clustering =
                 let row, col = neighbor.xy
                 let marker = labels.[row, col]
                 let height = uMatrix.[row, col]
-                if marker <> int Labels.Unlabeled && height < minNeighborHeight then
-                    minNeighbor <- neighbor
-                    minNeighborHeight <- height
+                if marker <> int Labels.Unlabeled then
+                    if height < minNeighborHeight then
+                        minNeighbor <- neighbor
+                        minNeighborHeight <- height
+                    // mis-classification:
+                    // if current point is x, two neighbors are y and z, and h(x) = h(y) = h(z)
+                    // but label(y) <> label(z), we need to re-label y and all its neighbors to match label(z)
+                    if height = curHeight then 
+                        misassignedLabels.Add(marker) |> ignore
 
                 match marker with 
                 | m when m = int Labels.Unlabeled -> nUnlabeled <- nUnlabeled + 1
                 | n when n = int Labels.Watershed -> nBorders <- nBorders + 1
-                | _ -> availableLabels.Add(marker)
+                | _ -> availableLabels.Add(marker) |> ignore
             
             let row, col = point.xy
-            if availableLabels.Count = 0 then
+            match availableLabels.Count with
+            | 0 ->
                 if nUnlabeled > 0 then
                     // new basin
                     basins <- basins + 1
                     labels.[row, col] <- basins
                     minimalpoints.[row, col] <- point
                     localmins.[basins] <- point
+                    labelToPoints.Add(basins, List<Point>())
+                    labelToPoints.[basins].Add(point)
                 else
                     // watershed
                     labels.[row, col] <- int Labels.Watershed
-                    minimalpoints.[row, col] <- minNeighbor
-            elif availableLabels.Count = 1 then
+                    minimalpoints.[row, col] <- minimalpoints.[minNeighbor.x, minNeighbor.y]
+                    labelToPoints.[int Labels.Watershed].Add(point)
+
+            | 1 -> 
                 // belong to an old basin
-                let marker = availableLabels.First()
+                let marker = availableLabels.Single()
                 labels.[row, col] <- marker
-                minimalpoints.[row, col] <- minNeighbor
-            else
-                // more than one label, hence - watershed!
-                labels.[row, col] <- int Labels.Watershed
+                minimalpoints.[row, col] <- minimalpoints.[minNeighbor.x, minNeighbor.y]
+                labelToPoints.[marker].Add(point)
+            | _ ->
+                // check if we have points that have been mis-classified (see comment above)
+                if misassignedLabels.Count > 1 && not (misassignedLabels.Contains (int Labels.Watershed)) then
+                    let newLabel = misassignedLabels.Min()
+                    let minPoint = labelToPoints.[newLabel].First()
+
+                    let reclassifyLabels = misassignedLabels.Where(fun l -> l <> newLabel).ToList()
+                    let reclassifyPoints = reclassifyLabels.SelectMany(fun l -> labelToPoints.[l].AsEnumerable()).ToList()
+                    labelToPoints.[newLabel].AddRange(reclassifyPoints)
+                    reclassifyLabels |> Seq.iter (fun key -> labelToPoints.Remove(key) |> ignore)
+                    reclassifyPoints |> 
+                        Seq.iter(
+                            fun p -> 
+                                labels.[p.x, p.y] <- newLabel
+                                minimalpoints.[p.x, p.y] <- minPoint)
+                    labels.[row, col] <- newLabel
+                    minimalpoints.[row, col] <- minPoint
+                    labelToPoints.[newLabel].Add(point)
+                else
+                    // more than one label, hence - watershed!
+                    labels.[row, col] <- int Labels.Watershed
+                    minimalpoints.[row, col] <- minimalpoints.[minNeighbor.x, minNeighbor.y]
+                    labelToPoints.[int Labels.Watershed].Add(point)
                 
                 
         /// Actually create the watershed
@@ -226,5 +267,5 @@ module Clustering =
                         let points = kvp.Value
                         points |> Seq.iter (fun p -> processShed (Point(p)))
             )
-   
+        
         member this.Labels = labels
